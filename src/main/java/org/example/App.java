d1ac8f6143f1a9395c2890e34c986a647c3ddb13
John Doe <bknudson117@gmail.com>
2021-05-31 01:44:01 -0400

    Exercise 3

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,21 @@
+package org.example;
+import java.util.Scanner;
+
+public class App 
+{
+    public static void main( String[] args ) {
+        Scanner input = new Scanner(System.in);
+        String noun, verb, adjective, adverb;
+        System.out.println("Enter a noun:");
+        noun = input.nextLine();
+        System.out.println("Enter a verb:");
+        verb = input.nextLine();
+        System.out.println("Enter an adjective");
+        adjective = input.nextLine();
+        System.out.println("Enter an adverb:");
+        adverb = input.nextLine();
+
+        System.out.printf("Do you %s your %s %s %s? That's hilarious!", verb, adjective, noun, adverb);
+
+    }
+}
